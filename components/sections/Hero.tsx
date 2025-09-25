872277a59c80b4dac55359bb9661977e839d4ce7
Alessandro Argenziano <1603alessandro@gmail.com>
2025-09-26 01:15:07 +0200

    update: did some small changes to the hero section

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -5,6 +5,8 @@ import { Button } from "@/components/ui/button";
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 
+import { CircleArrowRight } from "lucide-react";
+
 export default function Hero() {
   const ringRef = useRef<HTMLDivElement>(null);
 
@@ -23,14 +25,27 @@ export default function Hero() {
         ref={ringRef}
         className="pointer-events-none absolute inset-0 [mask-image:radial-gradient(600px_circle_at_50%_50%,#000_10%,transparent_60%)]"
       />
+      <motion.div
+        className="mx-auto mb-6 w-fit rounded-full flex items-center justify-center gap-2 py-2 px-4 bg-background-100 border-1 border-[#ffffff25] text-[#a0a0a0] text-base font-medium"
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.5, type: "spring", stiffness: 50 }}
+      >
+        <span className="text-gray-300">Want to work toghether?</span>
+        <CircleArrowRight size={20} />
+      </motion.div>
       <div className="mx-auto max-w-4xl text-center">
         <motion.h1
           className="text-4xl font-bold tracking-tight md:text-6xl"
           initial={{ y: 20, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
-          transition={{ type: "spring", stiffness: 80 }}
+          transition={{ type: "spring", stiffness: 60 }}
         >
-          Hi, I’m <span className="text-[#16b1ff]">Alessandro</span>.
+          Hi, I’m{" "}
+          <span className="bg-gradient-to-r from-[#ff6d92] to-[#45b2ff] bg-clip-text text-transparent">
+            Alessandro
+          </span>
+          .
         </motion.h1>
         <motion.p
           className="mt-4 text-muted-foreground md:text-lg"

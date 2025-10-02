24a0362d585ea5262ad01382e87ee45e041f8072
Alessandro Argenziano <1603alessandro@gmail.com>
2025-10-02 21:46:14 +0200

    feat: the center card in the bento grido is working as it should

--- components/sections/Grid.tsx
+++ components/sections/Grid.tsx
@@ -51,35 +51,37 @@ function CopyEmailCard() {
   }, []);
 
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center bg-cover bg-center">
-      <div className="h-40 w-full bg-blue absolute inset-0">
-        <SparklesCore
-          id="tsparticlesfullpage"
-          background="transparent"
-          minSize={0.3}
-          maxSize={1}
-          particleDensity={100}
-          className="w-full h-full"
-          particleColor="#FFFFFF"
-        />
-      </div>
-      <span className="relative text-[rgb(0,0,0,65%)] dark:text-[rgb(255,255,255,90%)] font-instrument-serif w-full max-w-80 -translate-y-4 py-2 text-center text-4xl font-bold text-balance">
-        Let's work together on your next project
-      </span>
-      <div className="relative flex">
-        <button
-          type="button"
-          onClick={handleCopy}
-          className="items-center gap-2 py-2 text-base font-light text-black dark:text-white/75 outline-hidden transition-all duration-300 cursor-pointer hover:text-black/60 dark:hover:text-white/90 flex w-full justify-center rounded-sm dark:bg-white/10 bg-black/10 px-8 border dark:border-white/10 border-black/10"
-          aria-live="polite"
-        >
-          {copied ? (
-            <CheckCheck className="h-5 w-5" aria-hidden="true" />
-          ) : (
-            <Copy className="h-5 w-5" aria-hidden="true" />
-          )}
-          {copied ? "Copied to clipboard" : EMAIL}
-        </button>
+    <div className="absolute inset-0 group">
+      <div className="flex h-full w-full flex-col items-center justify-center bg-cover bg-center">
+        <div className="h-40 w-full bg-blue absolute inset-0">
+          <SparklesCore
+            id="tsparticlesfullpage"
+            background="transparent"
+            minSize={0.3}
+            maxSize={1}
+            particleDensity={100}
+            className="w-full h-full"
+            particleColor="#FFFFFF"
+          />
+        </div>
+        <span className="w-full bg-linear-to-b from-black to-[#83d6ff90] bg-clip-text px-4 text-center text-3xl font-semibold tracking-normal text-transparent select-none dark:from-white max-w-80 -translate-y-4 py-2">
+          Let's work together on your next project
+        </span>
+        <div className="relative flex">
+          <button
+            type="button"
+            onClick={handleCopy}
+            className="items-center gap-2 py-2 text-base font-light text-black dark:text-white/75 outline-hidden transition-all duration-300 cursor-pointer hover:text-black/60 dark:hover:text-white/90 flex w-full justify-center rounded-sm dark:bg-white/10 bg-black/10 px-8 border dark:border-white/10 border-black/10 translate-y-4"
+            aria-live="polite"
+          >
+            {copied ? (
+              <CheckCheck className="h-5 w-5" aria-hidden="true" />
+            ) : (
+              <Copy className="h-5 w-5" aria-hidden="true" />
+            )}
+            {copied ? "Copied to clipboard" : EMAIL}
+          </button>
+        </div>
       </div>
     </div>
   );
@@ -168,7 +170,7 @@ const features = [
     cta: "Learn more",
     background: (
       <div className="flex h-full flex-col gap-10 py-12">
-        <h3 className="w-full bg-linear-to-b from-black to-[#16b1ff70] bg-clip-text px-4 text-center text-3xl font-semibold tracking-normal text-transparent select-none dark:from-white">
+        <h3 className="w-full bg-linear-to-b from-black to-[#83d6ff90] bg-clip-text px-4 text-center text-3xl font-semibold tracking-normal text-transparent select-none dark:from-white">
           I'm very flexible, <br /> i like Switzerland
         </h3>
         <div className="size-full flex items-center -left-35 -bottom-50">

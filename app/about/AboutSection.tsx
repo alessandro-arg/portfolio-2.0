877ba54dd0820239850c8449763c5a94576d3147
Alessandro Argenziano <1603alessandro@gmail.com>
2026-04-27 22:27:05 +0200

    feat: add corner bracket styles to enhance visual design in AboutSection

--- app/about/AboutSection.tsx
+++ app/about/AboutSection.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { AnimatedGradientText } from "@/components/ui/animated-gradient-text";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Plus } from "lucide-react";
 import SocialButtons from "./ui/social-buttons";
 import Image from "next/image";
 
@@ -10,7 +10,7 @@ export default function AboutSection() {
     <section id="about" className="relative px-4 pt-20 container">
       <div className="relative py-10 lg:max-h-[1300px]">
         <section className="relative mx-auto flex max-w-6xl flex-col items-center justify-center gap-8 lg:size-full lg:flex-row lg:justify-between">
-          <div className="min-w-full lg:max-w-[60%] lg:min-w-auto">
+          <div className="min-w-full lg:max-w-[55%] lg:min-w-auto">
             <p className="text-center lg:text-left mb-3 font-mono text-xs font-normal tracking-widest text-black/80 uppercase md:text-sm dark:text-white/70 md:mt-28">
               Know about me
             </p>
@@ -69,13 +69,34 @@ export default function AboutSection() {
             </a>
           </div>
 
-          <div className="relative grid aspect-square w-52 place-content-center lg:me-10 lg:mt-20 lg:w-[500px]">
+          <div className="group relative grid aspect-square w-52 place-content-center mr-4 lg:me-10 mt-20 lg:w-[500px] border-dotted border-2">
+            <div className="absolute -top-8 -left-8 border-b-2 border-r-2 border-t-0 border-l-0 border-dotted h-8 w-8 group-hover:-top-14 group-hover:-left-14 group-hover:h-14 group-hover:w-14 transition-all duration-400" />
+            <Plus
+              size={14}
+              className="absolute -bottom-2 -left-2 text-muted-foreground z-2 group-hover:rotate-45 transition-all duration-400"
+            />
+            <div className="absolute -bottom-8 -left-8 border-b-0 border-r-2 border-t-2 border-l-0 border-dotted h-8 w-8 group-hover:-bottom-14 group-hover:-left-14 group-hover:h-14 group-hover:w-14 transition-all duration-400" />
+            <Plus
+              size={14}
+              className="absolute -top-2 -right-2 text-muted-foreground z-2 group-hover:rotate-45 transition-all duration-400"
+            />
+            <div className="absolute -bottom-8 -right-8 border-b-0 border-r-0 border-t-2 border-l-2 border-dotted h-8 w-8 group-hover:-bottom-14 group-hover:-right-14 group-hover:h-14 group-hover:w-14 transition-all duration-400" />
+            <Plus
+              size={14}
+              className="absolute -top-2 -left-2 text-muted-foreground z-2 group-hover:rotate-45 transition-all duration-400"
+            />
+            <div className="absolute -top-8 -right-8 border-b-2 border-r-0 border-t-0 border-l-2 border-dotted h-8 w-8 group-hover:-top-14 group-hover:-right-14 group-hover:h-14 group-hover:w-14 transition-all duration-400" />
+            <Plus
+              size={14}
+              className="absolute -bottom-2 -right-2 text-muted-foreground z-2 group-hover:rotate-45 transition-all duration-400"
+            />
+
             {/* keep the exact “absolute + inset + rotate” look while using next/image */}
             <Image
               src="/images/profile-img.webp"
               alt="Alessandro pic"
               fill
-              className="absolute inset-0 rotate-3 rounded-[58px] object-cover"
+              className="absolute inset-0 rotate-3 sm:rotate-0 transition-all hover:rotate-3 duration-300 object-cover group-hover:scale-97"
               loading="lazy"
               decoding="async"
               sizes="80vw"

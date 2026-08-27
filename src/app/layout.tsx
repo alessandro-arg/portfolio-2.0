535df788601fc3d9ae4fde10c5bfa5f200211b88
Alessandro Argenziano <1603alessandro@gmail.com>
2026-08-27 08:51:36 +0200

    update: change the metadata in the layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: {
-    default: "Alessandro Argenziano",
+    default: "Alessandro Argenziano - Software Developer",
     template: "%s | Alessandro Argenziano",
   },
   description: "Software developer portfolio of Alessandro Argenziano.",
